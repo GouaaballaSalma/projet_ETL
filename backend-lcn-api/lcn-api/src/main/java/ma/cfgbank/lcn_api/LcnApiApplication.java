dbe8e5d796194276bfb53a3731c7b1736fbc653f
username <salma.gouaaballa@gmail.com>
2026-04-09 11:16:10 +0100

    ETL termine debut backend

---
+++ backend-lcn-api/lcn-api/src/main/java/ma/cfgbank/lcn_api/LcnApiApplication.java
@@ -0,0 +1,13 @@
+package ma.cfgbank.lcn_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcnApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcnApiApplication.class, args);
+	}
+
+}
---
+++ backend-lcn-api/lcn-api/src/test/java/ma/cfgbank/lcn_api/LcnApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.cfgbank.lcn_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcnApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
